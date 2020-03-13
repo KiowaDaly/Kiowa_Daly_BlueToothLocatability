8c5138a27f4806af6ae821f1ffd07d1bce888875
Kiowa Daly <kiowa.daly@ucdconnect.ie>
2020-03-13 13:09:22 +0000

    pre change to closest algorithm

--- app/src/main/java/com/kiowa/bluetoothlocatability/BluetoothLeService.kt
+++ app/src/main/java/com/kiowa/bluetoothlocatability/BluetoothLeService.kt
@@ -1,52 +1,87 @@
 package com.kiowa.bluetoothlocatability
 
-import android.app.IntentService
+
+import android.app.Service
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.le.BluetoothLeScanner
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanResult
+import android.bluetooth.le.*
 import android.content.Intent
-import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
-class BluetoothLeService(name: String?) : IntentService(name) {
-    private lateinit var bluetoothAdapter: BluetoothAdapter
-    private lateinit var bluetoothLeScanner: BluetoothLeScanner
-    private val formulas = Formulas()
 
-    override fun onHandleIntent(intent: Intent?) {
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
-        bluetoothLeScanner.startScan(scanCallback)
-        Log.i("BLE","HANDLEiNTENT")
+class BluetoothLeService() : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var bluetoothLeScanner: BluetoothLeScanner
+    private val radius  = (5.0)
+    private val formulas = Formulas()
+     private var hashMap  = HashMap<String,ArrayList<Double>>()
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i("BLE","Service accessed")
+        Log.i("BLE","Attempting to start scan")
+
+
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask(){
+            override fun run() {
+                bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+                bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+                val filter = ScanFilter.Builder().setDeviceName(null).build()
+                val settings = ScanSettings.Builder()
+                    .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                    .setReportDelay(1)
+                    .build()
+
+                val filters = ArrayList<ScanFilter>()
+                filters.add(filter)
+
+                bluetoothLeScanner.startScan(filters,settings,scanCallback)
+
+            }
+        },0,3000);
+        return START_STICKY
+    }
+    override fun onDestroy() {
+        bluetoothLeScanner.stopScan(scanCallback)
+        Log.i("BLE","Scan stopped ")
+    }
     private val scanCallback = object : ScanCallback() {
+
         override fun onScanResult(callbackType: Int, result: ScanResult) {
-//            val n = 4
-//            var distance = 0.0
-//            for (i in 0 until n) {
-//                val rssi = result.rssi.toDouble()
-//                val measuredPower = result.txPower.toDouble()
-//                distance += formulas.rssiDistanceFormula(rssi, measuredPower)
-//            }
-//            distance /= n
-//            val myBroadcast = Intent()
-//            myBroadcast.action = "DEVICE NEARBY!"
-//            val bundle = Bundle()
-//            bundle.putDouble("Distance", distance)
-//            bundle.putString("Beacon Name", result.device.name)
-//            myBroadcast.putExtra("Distance", distance)
-//            sendBroadcast(myBroadcast)
+            super.onScanResult(callbackType, result)
             Log.i("BLE_DETECTED",result.device.name)
         }
 
         override fun onBatchScanResults(results: List<ScanResult>) {
             super.onBatchScanResults(results)
-            //Do the triangulation in here! and return the closest device
-            for (result in results){
-                Log.i("BLE_DETECTED",result.device.name)
+            var closestVal = Double.MAX_VALUE
+            var closest = ""
+            for (result: ScanResult in results){
+                if(result.device.name != null){
+                    Log.i("BLE_DETECTED",result.device.name)
+                    val distance = formulas.rssiDistanceFormula(result.rssi.toDouble(), result.txPower.toDouble())
+                    if(distance<closestVal){
+                        closestVal = distance
+                        closest = result.device.name
+                    }
+                }
             }
+            if(closestVal < radius){
+                sendClosestDeviceBroadcast("You are in the:$closest",closest)
+            }else{
+                sendClosestDeviceBroadcast("Searching for nearby device....","no device found")
+            }
+
         }
 
         override fun onScanFailed(errorCode: Int) {
@@ -55,13 +90,29 @@ class BluetoothLeService(name: String?) : IntentService(name) {
         }
     }
 
-    /**
-     * The required methods for a service
-     *
-     *
-     *
-     *
-     *
-     */
+    fun sendClosestDeviceBroadcast(string:String, deviceName:String){
+        val intent = Intent("com.kgrjj.kiowa_daly_fyp.WithinRadius")
+        intent.putExtra("com.kiowa.EXTRA_TEXT",string)
+        intent.putExtra("DeviceName",deviceName)
+        sendBroadcast(intent)
+        Log.i("BLE","broadcast sent")
+    }
+    fun findClosest(){
+        var closestVal = Double.MAX_VALUE
+        var closest = ""
+
+        for((k,v) in hashMap){
+            val averageVal = v.average()
+            if(averageVal < closestVal){
+                closestVal = averageVal;
+                closest = k
+            }
+        }
+        if(closestVal < radius){
+            sendClosestDeviceBroadcast("You are in the:$closest",closest)
+        }else{
+            sendClosestDeviceBroadcast("Searching for nearby device....","no device found")
+        }
+    }
 
 }
--- app/src/main/java/com/kiowa/bluetoothlocatability/Formulas.kt
+++ app/src/main/java/com/kiowa/bluetoothlocatability/Formulas.kt
@@ -1,11 +1,20 @@
 package com.kiowa.bluetoothlocatability
 
+import android.util.Log
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.pow
+import kotlin.math.round
+
 class Formulas {
     fun rssiDistanceFormula(rssi: Double, P: Double): Double {
         //d = 10 ^ ((P-Rssi) / 10n) (n ranges from 2 to 4)
         val n = 2
-        val bottom = (10 * n).toDouble()
-        val top = P - rssi
-        return Math.pow(10.0, top / bottom)
+        val bottom = (10.0 * n)
+        val top = (P - (rssi))
+        val powVal = (top/bottom)
+        val value =  ((10.0).pow(powVal))
+        return value / 10.0.pow(9.0)
     }
 }
